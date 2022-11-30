1aa672b3aa731a533f4dbcaec98752f0e626bd61
Samsing1989 <112704225+Samsing1989@users.noreply.github.com>
2022-11-30 11:22:37 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+//Відео №2
+// //Вариант №1
+// console.log('Я вчу JS')
+// //Вариант №2
+// console.log('JS')
+// console.log('Учим')
+// //Вариант №3
+// console.log('Учим') console.log('JS')
+// //Вариант №4
+// console.log('Учим);
+// console.log('JS');
+//Відео №3
+// let userEyes;
+// let user;
+// let userName = 'Вася';
+// user = userName;
+// console.log(user);
+//Вариант №1
+// let user = 'Вася' let age = 36
+//Вариант №2
+// let myage = 36;
+//Вариант №3
+// const BLOCK_SIZE = 14 + 50;
+//Вариант №4
+// const BLOCK_HEIGHT = 100;
+//Вариант №5
+// if (true) {
+// 	var size = 15;
+// }
+// console.log(size);
+//Відео №4
+//Вариант №1
+// let userAge = 36;
+// let userInfo = "Фрилансер ${userAge}";
+//Вариант №2
+// let userHeight = 145 / 0;
+// //Вернется значение NaN
+// console.log(userHeight);
+//Вариант №3
+// let userName;
+// //Вернется тип данных Null
+// console.log(typeof userName);
+//Вариант №4
+// let userSize = "45" / "8";
+// //Вернется тип данных Number
+// console.log(typeof userSize);
